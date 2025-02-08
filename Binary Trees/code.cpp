ef19f840f6bc0c5361cf179c161662fecde751e9
Ankush Dixit <ankushdixit29@gmail.com>
2025-02-08 10:16:49 +0530

    tree

---
+++ Binary Trees/code.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+node* buildTree(node* root){
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"Enter data for inserting in the left of"<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"Enter data for inserting in the right of"<<data<<endl;
+    root->right=buildTree(root->right);
+    return root;
+}
+void levelOrderTraversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+        }
+        if(!q.empty()){
+            q.push(NULL);
+        } else{
+            cout<<temp->data<<" ";
+            if(temp->left)
+            q.push(temp->left);
+            if(temp->right)
+            q.push(temp->right);
+        }
+    }
+}
+void inorder(node* root){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void preorder(node* root){
+    if(root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(node* root){
+    if(root==NULL){
+        return ;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+int main() {
+    node* root=NULL;
+    root=buildTree(root);
+    return 0;
+}
